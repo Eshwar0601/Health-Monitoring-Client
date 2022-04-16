6ad4ac318949e64ea21f70fbadd35c9abb3d0358
Eshwar0601 <852eshwar@gmail.com>
2022-04-16 15:45:40 +0530

    Configure app to use local storage to store User JWT.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import {
-    useQuery,
-    useMutation,
-    useQueryClient,
-    QueryClient,
-    QueryClientProvider,
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
 } from "react-query";
 import { Table, Row, Col, Container } from "reactstrap";
 import { Bar, Line } from "react-chartjs-2";
@@ -13,169 +13,153 @@ import NavBar from "./NavBar";
 import SideBar from "./SideBar";
 
 const fetchPosts = async () => {
-    const data = await axios
-        .get("http://127.0.0.1:8080/api/posts/")
-        .then((res) => res)
-        .catch((err) => err);
-    return data;
+  const data = await axios
+    .get("http://127.0.0.1:8080/api/posts/", {
+      headers: {
+        "auth-token": localStorage.getItem("auth-token"),
+      },
+    })
+    .then((res) => res)
+    .catch((err) => err);
+  return data;
 };
 
 const Dashboard = () => {
-    const { data, error, isError, isLoading, status } = useQuery(
-        "posts",
-        fetchPosts
-    );
-    const [chartData, setChartData] = useState({});
-    const [heartData, setHeartData] = useState({});
-    if (status === "success") {
-        console.log("datttttttttttttttta", data.data["userData"]);
-    }
+  const { data, error, isError, isLoading, status } = useQuery(
+    "posts",
+    fetchPosts
+  );
+  const [chartData, setChartData] = useState({});
+  const [heartData, setHeartData] = useState({});
+  if (status === "success") {
+    console.log("datttttttttttttttta", data.data["userData"]);
+  }
 
-    const Chart = () => {
-        setChartData({
-            labels: [
-                "1:00 PM",
-                "2:00 PM",
-                "3:00 PM",
-                "4:00 PM",
-                "5:00 PM",
-                "6:00 PM",
-            ],
-            datasets: [
-                {
-                    label: "Temperature",
-                    data: [5, 22, 18, 19, 29, 30, 18, 24, 30, 21],
-                    backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-                    borderColor: ["rgba(255, 99, 132, 1)"],
-                    borderWidth: 1,
+  const Chart = () => {
+    setChartData({
+      labels: [
+        "1:00 PM",
+        "2:00 PM",
+        "3:00 PM",
+        "4:00 PM",
+        "5:00 PM",
+        "6:00 PM",
+      ],
+      datasets: [
+        {
+          label: "Temperature",
+          data: [5, 22, 18, 19, 29, 30, 18, 24, 30, 21],
+          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+          borderColor: ["rgba(255, 99, 132, 1)"],
+          borderWidth: 1,
+        },
+      ],
+    });
+
+    setHeartData({
+      labels: [
+        "1:00 PM",
+        "2:00 PM",
+        "3:00 PM",
+        "4:00 PM",
+        "5:00 PM",
+        "6:00 PM",
+        "7:00 PM",
+        "8:00 PM",
+        "9:00 PM",
+        "10:00 PM",
+      ],
+      datasets: [
+        {
+          label: "Heart Rate",
+          data: [91, 60, 99, 98, 98, 98, 70, 50, 90, 98],
+          backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+          borderColor: ["rgba(54, 162, 235, 1)"],
+          borderWidth: 1,
+        },
+      ],
+    });
+  };
+  useEffect(() => {
+    Chart();
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <Row>
+        <Col xs={2} lg={3} md={3} sm={3}>
+          <SideBar />
+        </Col>
+
+        <div className="col-lg-8">
+          <div>
+            <h1 className="text-white text-center my-5">Body Temperature</h1>
+            <Line
+              data={chartData}
+              options={{
+                responsive: true,
+                title: {
+                  text: "Body Temperature",
+                  display: true,
                 },
-            ],
-        });
-
-        setHeartData({
-            labels: [
-                "1:00 PM",
-                "2:00 PM",
-                "3:00 PM",
-                "4:00 PM",
-                "5:00 PM",
-                "6:00 PM",
-                "7:00 PM",
-                "8:00 PM",
-                "9:00 PM",
-                "10:00 PM",
-            ],
-            datasets: [
-                {
-                    label: "Heart Rate",
-                    data: [91, 60, 99, 98, 98, 98, 70, 50, 90, 98],
-                    backgroundColor: ["rgba(54, 162, 235, 0.2)"],
-                    borderColor: ["rgba(54, 162, 235, 1)"],
-                    borderWidth: 1,
+                scales: {
+                  yAxes: [
+                    {
+                      ticks: {
+                        beginAtZero: true,
+                      },
+                    },
+                  ],
                 },
-            ],
-        });
-    };
-    useEffect(() => {
-        Chart();
-    }, []);
+              }}
+            />
+            <h1 className="text-center my-5 text-white">Heart Rate :</h1>
+            <Line
+              data={heartData}
+              options={{
+                responsive: true,
+                title: { text: "Heart Rate", display: true },
+                scales: {
+                  yAxes: [
+                    {
+                      ticks: {
+                        beginAtZero: true,
+                      },
+                    },
+                  ],
+                },
+              }}
+            />
+          </div>
 
-    return (
-        <>
-            <NavBar />
-            <Row>
-                <Col xs={2} lg={3} md={3} sm={3}>
-                    <SideBar />
-                </Col>
-
-                <div className="col-lg-8">
-                    <div>
-                        <h1 className="text-white text-center my-5">
-                            Body Temperature
-                        </h1>
-                        <Line
-                            data={chartData}
-                            options={{
-                                responsive: true,
-                                title: {
-                                    text: "Body Temperature",
-                                    display: true,
-                                },
-                                scales: {
-                                    yAxes: [
-                                        {
-                                            ticks: {
-                                                beginAtZero: true,
-                                            },
-                                        },
-                                    ],
-                                },
-                            }}
-                        />
-                        <h1 className="text-center my-5 text-white">
-                            Heart Rate :
-                        </h1>
-                        <Line
-                            data={heartData}
-                            options={{
-                                responsive: true,
-                                title: { text: "Heart Rate", display: true },
-                                scales: {
-                                    yAxes: [
-                                        {
-                                            ticks: {
-                                                beginAtZero: true,
-                                            },
-                                        },
-                                    ],
-                                },
-                            }}
-                        />
-                    </div>
-
-                    <h1 className="text-center my-5 text-white">Health Data</h1>
-                    <Table striped bordered>
-                        <thead>
-                            <tr>
-                                <th className="text-center text-white">
-                                    Health ID
-                                </th>
-                                <th className="text-center text-white">
-                                    Body Temperature
-                                </th>
-                                <th className="text-center text-white">
-                                    Heart Rate
-                                </th>
-                                <th className="text-center text-white">
-                                    Time Stamp
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {status === "success" &&
-                                data.data["userData"].map((item) => (
-                                    <tr key={item._id}>
-                                        <td className="text-center">
-                                            {item._id}
-                                        </td>
-                                        <td className="text-center">
-                                            {item.temp}
-                                        </td>
-                                        <td className="text-center">
-                                            {item.hr}
-                                        </td>
-                                        <td className="text-center">
-                                            {item.date}
-                                        </td>
-                                    </tr>
-                                ))}
-                        </tbody>
-                    </Table>
-                    <div className="my-5">.</div>
-                </div>
-            </Row>
-        </>
-    );
+          <h1 className="text-center my-5 text-white">Health Data</h1>
+          <Table striped bordered>
+            <thead>
+              <tr>
+                <th className="text-center text-white">Health ID</th>
+                <th className="text-center text-white">Body Temperature</th>
+                <th className="text-center text-white">Heart Rate</th>
+                <th className="text-center text-white">Time Stamp</th>
+              </tr>
+            </thead>
+            <tbody>
+              {status === "success" &&
+                data.data["userData"].map((item) => (
+                  <tr key={item._id}>
+                    <td className="text-center">{item._id}</td>
+                    <td className="text-center">{item.temp}</td>
+                    <td className="text-center">{item.hr}</td>
+                    <td className="text-center">{item.date}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+          <div className="my-5">.</div>
+        </div>
+      </Row>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import {
-    Button,
-    Container,
-    Row,
-    Col,
-    Form,
-    FormGroup,
-    Label,
-    Input,
-    Card,
-    CardBody,
-    CardHeader,
+  Button,
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Card,
+  CardBody,
+  CardHeader,
 } from "reactstrap";
 import logo from "../logo.svg";
 import login from "../login.svg";
@@ -21,100 +21,93 @@ import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 
 const LoginComponent = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const loginData = {
-            email: email,
-            password: password,
-        };
-        const loginInfo = await axios
-            .post(`http://127.0.0.1:8080/api/user/login/`, loginData)
-            .then((res) => res)
-            .catch((err) => err);
-
-        if (loginInfo.status === 200) {
-            message.success("Login Successful");
-            navigate("/dashboard");
-        }
-        if (loginInfo.response.status === 400) {
-            message.error(loginInfo.response.data["error"]);
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const loginData = {
+      email: email,
+      password: password,
     };
-    return (
-        <div>
-            <NavBar />
-            <Container className="mt-5">
-                <Row>
-                    <Col lg={4} md={6} sm={12} className="mt-5 pt-5">
-                        <Card>
-                            <CardHeader className="card-primary">
-                                <img
-                                    src={avatar}
-                                    className="img img-fluid img-avatar mt-4 mb-4"
-                                    alt="login"
-                                />
-                            </CardHeader>
-                            <CardBody>
-                                <Form inline>
-                                    <FormGroup>
-                                        <Label for="exampleEmail" hidden>
-                                            Email
-                                        </Label>
-                                        <Input
-                                            id="exampleEmail"
-                                            value={email}
-                                            name="email"
-                                            placeholder="Email"
-                                            type="email"
-                                            required
-                                            onChange={(e) =>
-                                                setEmail(e.target.value)
-                                            }
-                                        />
-                                    </FormGroup>{" "}
-                                    <FormGroup>
-                                        <Label for="examplePassword" hidden>
-                                            Password
-                                        </Label>
-                                        <Input
-                                            id="examplePassword"
-                                            name="password"
-                                            placeholder="Password"
-                                            value={password}
-                                            type="password"
-                                            required
-                                            onChange={(e) =>
-                                                setPassword(e.target.value)
-                                            }
-                                        />
-                                    </FormGroup>{" "}
-                                    <Input
-                                        className="btn btn-primary btn-block btn-get-started text-white"
-                                        type="submit"
-                                        value="Login"
-                                        onClick={handleSubmit}
-                                    >
-                                        Submit
-                                    </Input>
-                                </Form>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col lg={8} md={6} sm={12} className="d-none d-md-block">
-                        <img
-                            src={login}
-                            className="img img-fluid d-block"
-                            alt="login"
-                        />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+    const loginInfo = await axios
+      .post(`http://127.0.0.1:8080/api/user/login/`, loginData)
+      .then((res) => res)
+      .catch((err) => err);
+
+    if (loginInfo.status === 200) {
+      localStorage.setItem("auth-token", loginInfo.data);
+      message.success("Login Successful");
+      navigate("/dashboard");
+    }
+    if (loginInfo.response.status === 400) {
+      message.error(loginInfo.response.data["error"]);
+    }
+  };
+  return (
+    <div>
+      <NavBar />
+      <Container className="mt-5">
+        <Row>
+          <Col lg={4} md={6} sm={12} className="mt-5 pt-5">
+            <Card>
+              <CardHeader className="card-primary">
+                <img
+                  src={avatar}
+                  className="img img-fluid img-avatar mt-4 mb-4"
+                  alt="login"
+                />
+              </CardHeader>
+              <CardBody>
+                <Form inline>
+                  <FormGroup>
+                    <Label for="exampleEmail" hidden>
+                      Email
+                    </Label>
+                    <Input
+                      id="exampleEmail"
+                      value={email}
+                      name="email"
+                      placeholder="Email"
+                      type="email"
+                      required
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </FormGroup>{" "}
+                  <FormGroup>
+                    <Label for="examplePassword" hidden>
+                      Password
+                    </Label>
+                    <Input
+                      id="examplePassword"
+                      name="password"
+                      placeholder="Password"
+                      value={password}
+                      type="password"
+                      required
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </FormGroup>{" "}
+                  <Input
+                    className="btn btn-primary btn-block btn-get-started text-white"
+                    type="submit"
+                    value="Login"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </Input>
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg={8} md={6} sm={12} className="d-none d-md-block">
+            <img src={login} className="img img-fluid d-block" alt="login" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default LoginComponent;
