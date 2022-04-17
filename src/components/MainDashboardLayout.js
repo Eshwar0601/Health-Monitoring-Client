6f3fbac3253e5923f0fb45f48769c1145725a27d
Eshwar0601 <852eshwar@gmail.com>
2022-04-17 16:39:17 +0530

    added logout functionality.

--- src/components/MainDashboardLayout.js
+++ src/components/MainDashboardLayout.js
@@ -3,32 +3,31 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import SideBar from "./SideBar";
 import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
+    useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
 } from "react-query";
 import { Col, Row } from "reactstrap";
 
 const MainDashboardLayout = () => {
-  const queryClient = new QueryClient();
+    const queryClient = new QueryClient();
 
-  return (
-    <>
-      <NavBar />
-      <Row>
-        <Col xs={2} lg={3} md={3} sm={3}>
-          <SideBar />
-        </Col>
-        <div className="col-lg-8">
-          <QueryClientProvider client={queryClient}>
-            <Outlet />
-          </QueryClientProvider>
-        </div>
-      </Row>
-    </>
-  );
+    return (
+        <>
+            <Row>
+                <Col xs={2} lg={3} md={3} sm={3}>
+                    <SideBar />
+                </Col>
+                <div className="col-lg-8">
+                    <QueryClientProvider client={queryClient}>
+                        <Outlet />
+                    </QueryClientProvider>
+                </div>
+            </Row>
+        </>
+    );
 };
 
 export default MainDashboardLayout;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,70 +1,84 @@
 import React from "react";
 import {
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarFooter,
-  CDBSidebarHeader,
-  CDBSidebarMenu,
-  CDBSidebarMenuItem,
+    CDBSidebar,
+    CDBSidebarContent,
+    CDBSidebarFooter,
+    CDBSidebarHeader,
+    CDBSidebarMenu,
+    CDBSidebarMenuItem,
 } from "cdbreact";
 import { NavLink } from "react-router-dom";
 
 const SideBar = () => {
-  return (
-    <>
-      <div className="stickySidebar">
-        <CDBSidebar textColor="#fff" backgroundColor="#333">
-          <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-            <a
-              href="/dashboard"
-              className="text-decoration-none"
-              style={{ color: "inherit" }}
-            >
-              Dashboard
-            </a>
-          </CDBSidebarHeader>
-          <CDBSidebarContent className="sidebar-content">
-            <CDBSidebarMenu>
-              <NavLink exact to="/" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="columns">Home</CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink
-                exact
-                to="/dashboard/tables"
-                activeClassName="activeClicked"
-              >
-                <CDBSidebarMenuItem icon="table">
-                  Periodical Health Data
-                </CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink
-                exact
-                to="/dashboard/visualization"
-                activeClassName="activeClicked"
-              >
-                <CDBSidebarMenuItem icon="user">
-                  Graphical Data
-                </CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink exact to="/analytics" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="chart-line">
-                  Prediction and Analytics
-                </CDBSidebarMenuItem>
-              </NavLink>
+    return (
+        <>
+            <div className="stickySidebar">
+                <CDBSidebar textColor="#fff" backgroundColor="#333">
+                    <CDBSidebarHeader
+                        prefix={<i className="fa fa-bars fa-large"></i>}
+                    >
+                        <a
+                            href="/dashboard"
+                            className="text-decoration-none"
+                            style={{ color: "inherit" }}
+                        >
+                            Dashboard
+                        </a>
+                    </CDBSidebarHeader>
+                    <CDBSidebarContent className="sidebar-content">
+                        <CDBSidebarMenu>
+                            <NavLink
+                                exact
+                                to="/"
+                                activeClassName="activeClicked"
+                            >
+                                <CDBSidebarMenuItem icon="columns">
+                                    Home
+                                </CDBSidebarMenuItem>
+                            </NavLink>
+                            <NavLink
+                                exact
+                                to="/dashboard/tables"
+                                activeClassName="activeClicked"
+                            >
+                                <CDBSidebarMenuItem icon="table">
+                                    Periodical Health Data
+                                </CDBSidebarMenuItem>
+                            </NavLink>
+                            <NavLink
+                                exact
+                                to="/dashboard/visualization"
+                                activeClassName="activeClicked"
+                            >
+                                <CDBSidebarMenuItem icon="user">
+                                    Graphical Data
+                                </CDBSidebarMenuItem>
+                            </NavLink>
+                            <NavLink
+                                exact
+                                to="/analytics"
+                                activeClassName="activeClicked"
+                            >
+                                <CDBSidebarMenuItem icon="chart-line">
+                                    Prediction and Analytics
+                                </CDBSidebarMenuItem>
+                            </NavLink>
 
-              <NavLink
-                exact
-                to="/hero404"
-                target="_blank"
-                activeClassName="activeClicked"
-              >
-                <CDBSidebarMenuItem icon="exclamation-circle">
-                  Logout
-                </CDBSidebarMenuItem>
-              </NavLink>
-            </CDBSidebarMenu>
-          </CDBSidebarContent>
-          <CDBSidebarFooter style={{ textAlign: "center" }}>
+                            <NavLink
+                                exact
+                                to="/"
+                                activeClassName="activeClicked"
+                                onClick={() =>
+                                    localStorage.removeItem("auth-token")
+                                }
+                            >
+                                <CDBSidebarMenuItem icon="exclamation-circle">
+                                    Logout
+                                </CDBSidebarMenuItem>
+                            </NavLink>
+                        </CDBSidebarMenu>
+                    </CDBSidebarContent>
+                    {/* <CDBSidebarFooter style={{ textAlign: "center" }}>
             <div
               className="sidebar-btn-wrapper"
               style={{
@@ -73,11 +87,11 @@ const SideBar = () => {
             >
               Sidebar Footer
             </div>
-          </CDBSidebarFooter>
-        </CDBSidebar>
-      </div>
-    </>
-  );
+          </CDBSidebarFooter> */}
+                </CDBSidebar>
+            </div>
+        </>
+    );
 };
 
 export default SideBar;
